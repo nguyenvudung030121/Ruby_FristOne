3a3f32275864cfe5feb5786206011edebff93f1a
Vudung <nguyenvudung030121@gmail.com>
2022-08-26 17:37:00 +0700

    new

--- test.rb
+++ test.rb
@@ -1,1 +1,11 @@
-puts 5+5
+puts "Xin Chào: Nguyễn Vũ Dũng - 1911505310109"
+print "Nhap vao so 1: "
+number_1 = gets.to_i
+print "Nhap vao so 2: "
+number_2 = gets.to_i
+puts "Số nhập vào #{number_1} - #{number_2} "
+if number_1 > number_2 
+    puts "So lon nhat la #{number_1} "
+else
+    puts "So lon nhat la #{number_2} "
+end
