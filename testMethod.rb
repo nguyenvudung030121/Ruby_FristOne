ceeaa033d450938c50036c8f8ccd1e8c4384b165
Vudung <nguyenvudung030121@gmail.com>
2022-09-08 20:40:36 +0700

    new

--- testMethod.rb
+++ testMethod.rb
@@ -4,3 +4,13 @@ end
 
 print_Name
 print_Name "Nguyen Dung"
+
+def sum_2_variable (a=0,b=0)
+    puts "Result #{a} + #{b} = #{a+b}"
+end 
+
+def subtraction_2_variable(c,d)
+    c-d
+end
+sum_2_variable(4,6)
+puts subtraction_2_variable 3,2
