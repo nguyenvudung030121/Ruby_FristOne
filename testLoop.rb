d5773a005db47e4cfb277262fec664a0567220a5
Vudung <nguyenvudung030121@gmail.com>
2022-09-08 22:20:36 +0700

    new

---
+++ testLoop.rb
@@ -0,0 +1,29 @@
+#########################
+(1..5).each do |counter|
+  puts "Giá trị của counter: #{counter}"
+end
+
+###################
+for i in 0..5
+   puts "Giá trị của biến i là: #{i}"
+end
+
+for i in 0..5
+    if i == 2 then
+        next
+    end
+    puts "Giá trị của biến i là #{i}"
+end
+
+
+############################
+counter = 0
+until counter > 5  do
+   puts("Giá trị của biến counter_until là #{counter += 1}" )
+end
+counter = 0
+
+############################
+begin
+   puts "Giá trị của biến counter là #{counter += 1}"
+end while counter > 5
