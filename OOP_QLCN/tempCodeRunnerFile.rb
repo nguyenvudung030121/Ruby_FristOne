078ae17e741a9fe5fb2db205de6d46c2135fa697
Vudung <nguyenvudung030121@gmail.com>
2022-09-30 10:55:50 +0700

    30/9

---
+++ OOP_QLCN/tempCodeRunnerFile.rb
@@ -0,0 +1,11 @@
+when 2      
+
+    #     offices << engineer2
+
+    #     len = offices.length
+
+    #     for i in 0..len
+    #         puts offices[i]
+    #     end
+    # else break
+    # end  
